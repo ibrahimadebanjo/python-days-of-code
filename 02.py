ab54253bad8f18f2684f584bcd7102efd27492fc
Ibrahim Adebanjo <ibrahimadebanjo283@gmail.com>
2025-03-28 14:54:58 -0700

    first documentation

---
+++ 02.py
@@ -0,0 +1,16 @@
+import math
+pi = 3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(pow(pi, 4))
+print(math.sqrt(pi))
+print(math.sqrt(pi))
+name = "Ibrahim Adebanjo"
+print(name[0:6])
+print(name[:6])
+print(name[6:])
+print(name[::3])
+website = "https://google.com"
+print(website[slice(8,-4)])
+
---
+++ app.py
@@ -0,0 +1,23 @@
+name = "Ibrahim Adebanjo"
+print(name)
+print(type(name))
+x = 21
+x += 1
+print(x)
+print(type(x)) 
+print("I " + name + " is " +  str(x))
+# float
+weight = 56.4
+print(weight)
+print(type(weight))
+print("My weight is : " + str(weight))
+# Boolean
+animal = True
+print("is that an animal ? " + str(animal))
+name, age, handsome = "Ibrahim", 22, True
+print(name, age, handsome)
+name = input("what's your name?")
+print('hello ' + name)
+age = int(input("what's your age?"))
+age += 1
+print(age)
